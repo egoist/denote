d7a888b7003cbeb5e6a63bc5d7cb5cd429078a29
EGOIST <0x142857@gmail.com>
2019-09-13 17:02:58 +0800

    tweak html meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,19 @@ module.exports = {
   mode: 'spa',
   head: {
     title: 'Denote',
+    meta: [
+      {
+        charset: 'UTF-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0'
+      },
+      {
+        'http-equiv': 'X-UA-Compatible',
+        content: 'ie=edge'
+      }
+    ],
     link: [
       {
         rel: 'icon',
